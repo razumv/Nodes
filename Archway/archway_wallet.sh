9fda661327376edad5fc0dfc3f0401ffdf66b56f
KURASH <92112412+kuraassh@users.noreply.github.com>
2022-03-22 16:37:15 +0200

    fix

--- Archway/archway_wallet.sh
+++ Archway/archway_wallet.sh
@@ -3,6 +3,6 @@ cd $HOME
 source $HOME/.profile
 ARCHWAY_ADDR=$(archwayd keys show $ARCHWAY_NODENAME -a)
 echo 'export ARCHWAY_ADDR='${ARCHWAY_ADDR} >> $HOME/.profile
-CELESTIA_VALOPER=$(archwayd keys show $ARCHWAY_NODENAME --bech val -a)
+ARCHWAY_VALOPER=$(archwayd keys show $ARCHWAY_NODENAME --bech val -a)
 echo 'export ARCHWAY_VALOPER='${ARCHWAY_VALOPER} >> $HOME/.profile
 source $HOME/.profile
