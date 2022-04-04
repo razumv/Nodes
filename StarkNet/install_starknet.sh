9e6dfb381a741b67ec45b4f5ffb5549fdb4fc152
KURASH <92112412+kuraassh@users.noreply.github.com>
2022-04-04 15:38:48 +0300

    fix

--- StarkNet/install_starknet.sh
+++ StarkNet/install_starknet.sh
@@ -12,28 +12,28 @@ echo 'export ALCHEMY_KEY='$ALCHEMY_KEY >> $HOME/.bash_profile
 echo "Устанавливаем софт"
 echo "-----------------------------------------------------------------------------"
 
-sudo apt update -y
-sudo apt install build-essential libssl-dev libffi-dev python3-dev screen git python3-pip python3.8-venv -y
-sudo apt-get install libgmp-dev -y
-pip3 install fastecdsa
-sudo apt-get install -y pkg-config
-curl -s https://raw.githubusercontent.com/razumv/helpers/main/tools/install_ufw.sh | bash
-curl -s https://raw.githubusercontent.com/razumv/helpers/main/tools/install_rust.sh | bash
-rustup update stable
-source $HOME/.cargo/env
+sudo apt update -y &>/dev/null
+sudo apt install build-essential libssl-dev libffi-dev python3-dev screen git python3-pip python3.8-venv -y &>/dev/null
+sudo apt-get install libgmp-dev -y &>/dev/null
+pip3 install fastecdsa &>/dev/null
+sudo apt-get install -y pkg-config &>/dev/null
+curl -s https://raw.githubusercontent.com/razumv/helpers/main/tools/install_ufw.sh | bash &>/dev/null
+curl -s https://raw.githubusercontent.com/razumv/helpers/main/tools/install_rust.sh | bash &>/dev/null
+rustup update stable &>/dev/null
+source $HOME/.cargo/env &>/dev/null
 sleep 1
 echo "Весь необходимый софт установлен"
 echo "-----------------------------------------------------------------------------"
 
-git clone --branch v0.1.6-alpha https://github.com/eqlabs/pathfinder.git
-cd pathfinder/py
-python3 -m venv .venv
-source .venv/bin/activate
-PIP_REQUIRE_VIRTUALENV=true pip install --upgrade pip
-PIP_REQUIRE_VIRTUALENV=true pip install -r requirements-dev.txt
-cargo build --release --bin pathfinder
+git clone --branch v0.1.6-alpha https://github.com/eqlabs/pathfinder.git &>/dev/null
+cd pathfinder/py &>/dev/null
+python3 -m venv .venv &>/dev/null
+source .venv/bin/activate &>/dev/null
+PIP_REQUIRE_VIRTUALENV=true pip install --upgrade pip &>/dev/null
+PIP_REQUIRE_VIRTUALENV=true pip install -r requirements-dev.txt &>/dev/null
+cargo build --release --bin pathfinder &>/dev/null
 sleep 2
-source $HOME/.bash_profile
+source $HOME/.bash_profile &>/dev/null
 echo "Билд завершен успешно"
 echo "-----------------------------------------------------------------------------"
 
@@ -51,8 +51,8 @@ After=network.target
 Type=simple
 User=$USER
 WorkingDirectory=$HOME/pathfinder/py
-Environment=PATH="$HOME/pathfinder/py/.venv/bin:$PATH"
-ExecStart=$HOME/pathfinder/target/release/pathfinder --http-rpc=\"0.0.0.0:9545\" --ethereum.url $ALCHEMY_KEY\"
+Environment=PATH="$HOME/pathfinder/py/.venv/bin:\$PATH"
+ExecStart=$HOME/pathfinder/target/release/pathfinder --ethereum.url $ALCHEMY_KEY
 Restart=always
 RestartSec=10
 LimitNOFILE=10000
@@ -64,10 +64,10 @@ EOF
 echo "Сервисные файлы созданы успешно"
 echo "-----------------------------------------------------------------------------"
 
-sudo systemctl restart systemd-journald
-sudo systemctl daemon-reload
-sudo systemctl enable starknet
-sudo systemctl restart starknet
+sudo systemctl restart systemd-journald &>/dev/null
+sudo systemctl daemon-reload &>/dev/null
+sudo systemctl enable starknet &>/dev/null
+sudo systemctl restart starknet &>/dev/null
 
 echo "Нода добавлена в автозагрузку на сервере, запущена"
 echo "-----------------------------------------------------------------------------"
