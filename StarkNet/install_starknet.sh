25a92d0dcba963ce9d3e38c26948ab57f1e376d7
KURASH <92112412+kuraassh@users.noreply.github.com>
2022-04-05 14:10:28 +0300

    fix

--- StarkNet/install_starknet.sh
+++ StarkNet/install_starknet.sh
@@ -13,7 +13,7 @@ echo "Устанавливаем софт"
 echo "-----------------------------------------------------------------------------"
 
 sudo apt update -y &>/dev/null
-sudo apt install build-essential libssl-dev libffi-dev python3-dev screen git python3-pip python3.8-venv -y &>/dev/null
+sudo apt install build-essential libssl-dev libffi-dev python3-dev screen git python3-pip python3.*-venv -y &>/dev/null
 sudo apt-get install libgmp-dev -y &>/dev/null
 pip3 install fastecdsa &>/dev/null
 sudo apt-get install -y pkg-config &>/dev/null
