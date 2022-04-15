b4b9af9aad6e79ce105defd59c0e0a180fabda0e
KURASH <92112412+kuraassh@users.noreply.github.com>
2022-04-15 13:28:16 +0300

    fix

--- StarkNet/update_starknet.sh
+++ StarkNet/update_starknet.sh
@@ -5,12 +5,18 @@ curl -s https://raw.githubusercontent.com/razumv/helpers/main/doubletop.sh | bas
 echo "-----------------------------------------------------------------------------"
 echo "Начинаем обновление репрозитория "
 echo "-----------------------------------------------------------------------------"
-cd ~/pathfinder
+cd ~/pathfinder/py
 git fetch &>/dev/null
 git checkout v0.1.8-alpha &>/dev/null
 echo "Репозиторий успешно обновлен, начинаем билд"
 echo "-----------------------------------------------------------------------------"
+python3 -m venv .venv &>/dev/null
+source .venv/bin/activate &>/dev/null
+PIP_REQUIRE_VIRTUALENV=true pip install --upgrade pip &>/dev/null
+PIP_REQUIRE_VIRTUALENV=true pip install -r requirements-dev.txt &>/dev/null
 cargo build --release --bin pathfinder &>/dev/null
+sleep 2
+source $HOME/.bash_profile &>/dev/null
 echo "Билд завершен успешно"
 echo "-----------------------------------------------------------------------------"
 systemctl restart starknet
