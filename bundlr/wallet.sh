b0e8f0cf4725b826ed7d1dffaa8f9a98cef448de
KURASH <92112412+kuraassh@users.noreply.github.com>
2022-07-23 12:39:51 +0300

    upd

--- bundlr/wallet.sh
+++ bundlr/wallet.sh
@@ -11,16 +11,5 @@ sleep 1
 BUNDLR_PORT=2109
 echo "export BUNDLR_PORT="${BUNDLR_PORT}"" >> $HOME/.profile
 echo 'export BUNDLR_ADDR='$BUNDLR_ADDR >> $HOME/.profile
-echo "-----------------------------------------------------------------------------"
-sudo tee <<EOF >/dev/null $HOME/bundlr/validator-rust/.env
-PORT=${BUNDLR_PORT}
-VALIDATOR_KEY=./wallet.json
-BUNDLER_URL=https://testnet1.bundlr.network
-GW_WALLET=./wallet.json
-GW_CONTRACT=RkinCLBlY4L5GZFv8gCFcrygTyd5Xm91CzKlR6qxhKA
-GW_ARWEAVE=https://arweave.testnet1.bundlr.network
-EOF
-cd $HOME/bundlr/validator-rust && docker-compose up -d
-
-echo "BUNDLR успешно установлен, проверьте логи командой docker-compose -f $HOME/bundlr/validator-rust logs -f --tail=100"
+echo "Ваш кошелек добавлен в систему в виде переменной"
 echo "-----------------------------------------------------------------------------"
